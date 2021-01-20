18c4fc31f7607f05c8265c393b713aa9d9c37215
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2021-01-19 23:17:25 -0500

    PEP8 formatted

--- Luis_Guzman_Package/helper_functions.py
+++ Luis_Guzman_Package/helper_functions.py
@@ -4,24 +4,26 @@
 import pandas as pd
 import numpy as np
 
+
 def null_count(df):
-  count = df.isnull().sum().sum()
-  return int(count)
+    count = df.isnull().sum().sum()
+    return int(count)
+
 
 def abbr_2_st(state_series, abbr_2_st=True):
-  abbreviation = {
-    'Alabama': 'AL',
-    'AL': 'Alabama',
-    'Arizona': 'AZ',
-    'AZ': 'Arizona',
-    'California': 'CA',
-    'CA': 'California',
-    'Delaware': 'DE',
-    'DE': 'Delaware',
-    'Ohio': 'OH',
-    'OH': 'Ohio'
-  }
+    abbreviation = {
+        'Alabama': 'AL',
+        'AL': 'Alabama',
+        'Arizona': 'AZ',
+        'AZ': 'Arizona',
+        'California': 'CA',
+        'CA': 'California',
+        'Delaware': 'DE',
+        'DE': 'Delaware',
+        'Ohio': 'OH',
+        'OH': 'Ohio'
+    }
 
-  for state in state_series:
-    abb =print(abbreviation[state])
-  return abb
+    for state in state_series:
+        abb = print(abbreviation[state])
+        return abb
